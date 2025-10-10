ba8d2ba507696738ff59bd6ce53ccb301cf23168
JessielynJenisha30 <220701901@rajalakshmi.edu.in>
2025-10-10 18:34:10 +0530

    Create main.tf

---
+++ campus-parking/infra/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "campus-parking-rg"
+    storage_account_name = "campusparkstate001"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
